1c694ec0d8efad6feea930febb223e07b6827a0c
digitalraiz <umadeportes1@gmail.com>
2021-09-30 13:19:59 +0530

    First flutter app push to git commit test 3

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'sample application from my side'),
+      home: const MyHomePage(title: 'sample application'),
     );
   }
 }
